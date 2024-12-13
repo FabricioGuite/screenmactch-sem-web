458467d14a54ed8c001d5e9f85f10b182fd3a97f
Fabricio Pereira <fabricio.pereira@fiscaltech.com.br>
2024-12-13 08:01:40 -0300

    commit teste

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -51,6 +51,8 @@ public class Principal {
         dadosEpisodios.stream().filter(e -> !e.avaliacao().equalsIgnoreCase("N/A")).
             sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed()).
             limit(5).forEach(System.out::println);
+        
+        System.out.println("\n");
 
         List<Episodio> episodios = temporadas.stream()
         .flatMap(t -> t.episodios().stream().map(d -> new Episodio(t.numero(), d))).collect(Collectors.toList());
